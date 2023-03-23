81500cf1d91b682a18cdc40fad811079f52f40a4
Renata <n.gg.1309@gmail.com>
2023-03-23 14:28:19 -0500

    Initial Commit

---
+++ SpoonacularApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpoonacularApp
+//
+//  Created by Natalia Goyes on 23/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
